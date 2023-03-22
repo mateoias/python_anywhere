d4605accd87f0628ac4eeb323339421178b9e90b
mateoias1 <mateoias@hotmail.com>
2023-03-22 23:33:13 +0000

    updating to prompt version 3 (gpt-3.5-turbo)

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
 from config import app, login
-from flask import render_template, flash, redirect, url_for, request, send_file
+from flask import render_template, flash, redirect, url_for, request, make_response
 # from flask_migrate import Migrate
 from forms import LoginForm,  RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from models import User, db
-from prompts_v2 import prompt_maker, model_builder, question_writer
+from prompts_v3 import prompt_maker, model_builder, question_writer
 # migrate = Migrate(app,db)
 login.login_view = 'login'
 
@@ -23,7 +23,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password', category = 'error')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -47,7 +47,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
+        flash('Congratulations, you are now a registered user! Please sign in.', category = 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -67,14 +67,13 @@ def articles():
             user_input.append(item)
         genre = user_input[0][1]
         topic = user_input[1][1]
-        # words = user_input[2][1]
-        # questions = user_input[3][1]
+        words = user_input[2][1]
         topic.strip()
-        if user_input[2][1] =="on":
-            questions = True
+        words.strip()
         if len(user_input)>3:
-            words = user_input[3][1]
-            words.strip()
+            if user_input[3][1] == "on":
+                questions=True
+
         prompt = prompt_maker(genre, topic, words)
         text= model_builder(genre, prompt)
         if questions ==True:
@@ -83,27 +82,19 @@ def articles():
             question_text = model_builder("write_questions", questions_prompt)
             return render_template('articles.html', genre = genre, topic = topic, main_text = text, question_text = question_text)
         else:
-            return render_template('articles.html', genre = genre, topic = topic, main_text = text, question_text = "")
+            question_text = " "
+            return render_template('articles.html', genre = genre, topic = topic, main_text = text, question_text = question_text)
 
-# @app.route('/download')
-# def download_file():
-#     return render_template('index.html', title='Home Page')
-	#path = "html2pdf.pdf"
-	#path = "info.xlsx"
-	#path = "simple.docx"
-# 	path = "sample.txt"
-# 	return send_file(path, as_attachment=True)
-# <!--        To download the text click here-->
-# <!--        <p>-->
-# <!--	<a href="{{ url_for('download_file') }}">Download</a>-->
-# <!--</p>-->
+
+
+
+@app.route('/download_text', methods=['GET', 'POST'])
+def download_text():
+    text = request.form['text']
+    response = make_response(text)
+    response.headers.set('Content-Disposition', 'attachment', filename='text.txt')
+    response.headers.set('Content-Type', 'text/plain')
+    return response
 
 # for dialog, temperature high (0.95) frequence penalty high (0.75)
 # for article temperature(0.7), frequency penalty 0
-
-# @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
-# def download(filename):
-#     # Appending app path to upload folder path within app root folder
-#     uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
-#     # Returning file from appended path
-#     return send_from_directory(directory=uploads, filename=filename)
--- prompts_v3.py
+++ prompts_v3.py
@@ -1,12 +1,12 @@
+# prompt maker modified to use gpt-3.5-turbo
+
 import openai
 import os
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def prompt_maker(genre, topic, words):
-    # if len(words)>0:
-    prompt = f"""write a very long and easy {genre} about {topic} using these words: {words}"""
-    # else:
-    #     prompt = f"""write a very long and easy {genre} about {topic}"""
+    prompt = f"""You are a language teacher and writer who writes articles for your students. Write a  long and easy {genre} about {topic} for your students. Include these words: {words}"""
+
 
     return prompt
 # add "simple" to the prompt get that Taiwan style writing
@@ -16,7 +16,7 @@ def prompt_maker(genre, topic, words):
 
 
 def question_writer(genre,text):
-    questions_prompt = f"""{text}
+    questions_prompt = f"""{text} You are a language teacher and writer who writes articles for your students.
 Write 8 multiple choice TOEIC-type questions with 4 answer choices about the {genre}:
 Questions:"""
     print("questions_prompt", questions_prompt)
@@ -35,10 +35,10 @@ def model_builder(genre, prompt):
     else:
         temperature = 0.8
         frequency_penalty = 0.25
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        # model = "text-curie-001",
-        prompt = prompt,
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages = [{"role": "system", "content" : "You are a language teacher and writer who writes articles for your students"},
+        {"role": "user", "content" : prompt}],
         temperature=temperature,
         max_tokens=1000,
         top_p=1.0,
@@ -46,5 +46,5 @@ def model_builder(genre, prompt):
         presence_penalty=0.0
         )
     print (response)
-    text = response.choices[0].text
+    text = response.choices[0].message.content
     return text
